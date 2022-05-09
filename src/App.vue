048977c0727f8b667fa585c4746800a8230103d0
ariannagreco3393 <a.greco3393@gmail.com>
2022-05-09 13:17:30 +0200

    add milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,67 @@
 <template>
   <div id="app">
-    <form action="search" method="get">
-      <input placeholder="Cerca per titolo" type="text" value="">
-      <button type="submit">Cerca</button>
-    </form>
-    <div class="film">
-      <ul v-for="result in results" :key="result.id">
-        <li>{{}}</li>
-        <li></li>
-        <li></li>
-        <li></li>
+    <header>
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-6">
+            <div class="logo">
+              <img src="@/assets/img/boolflix-logo.png" alt="" />
+            </div>
+          </div>
+          <div class="col-6">
+            <form action="#" method="get" @submit.prevent="searchFilm">
+              <input
+                placeholder="Cosa vuoi guardare?"
+                type="text"
+                name="search"
+                id="search"
+                v-model="searchText"
+              />
+              <button type="submit">Cerca</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </header>
+    <main>
+      <ul>
+        <li v-for="film in films" :key="film.id">
+          <p>{{ film.title }}</p>
+          <p>{{ film.original_title }}</p>
+          <p>{{ film.original_language }}</p>
+          <p>{{ film.vote_average }}</p>
+        </li>
       </ul>
-    </div>
+    </main>
   </div>
 </template>
 
 <script>
-
-import axios from "axios"
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    
-  },
+  name: "App",
   data() {
     return {
-      API_URL: 'https://api.themoviedb.org/3/search/movie?api_key=be95f9eaaa0cffacac52f868a0272550&language=it-IT&page=1&include_adult=false&query=star wars',
-      results: null,
-      loading: true,
-      error: null
-    }
+      searchText: "",
+      films: null,
+    };
   },
   methods: {
-    callApi() {
+    searchFilm() {
       axios
-      .get(this.API_URL)
-      .then((response) => {
-        this.results = response.data
-        this.loading = false
-
-      })
-      .catch((error) => {
-        console.error();
-        error;
-        this.error = 'Sorry there is a problem ${error}'
-      })
-
-    }
-  }
-}
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=be95f9eaaa0cffacac52f868a0272550&language=it-IT&page=1&include_adult=false&query=${this.searchText}`
+        )
+        .then((response) => {
+          this.films = response.data.results;
+        });
+    },
+  },
+  components: {},
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
+@import "@/assets/style.scss"
+
 </style>
